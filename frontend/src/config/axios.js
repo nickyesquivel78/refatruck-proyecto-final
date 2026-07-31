40c4012ea6e0eef5c9429e2e9204f1c74bc13b8e
Nicky <nickyesquivel027@gmail.com>
2026-07-31 14:57:23 -0600

    sec: interceptor de Axios para inyeccion de token JWT

---
+++ frontend/src/config/axios.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const clienteAxios = axios.create({
+    // Asegúrate de que esta sea la URL correcta de tu backend en Laravel
+    baseURL: 'http://localhost:8000/api' 
+});
+
+// "Interceptor": Antes de que cualquier petición salga hacia Laravel, 
+// Axios revisará si tienes un Token guardado y lo adjuntará en los "Headers" (cabeceras).
+clienteAxios.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('token');
+        
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+export default clienteAxios;
